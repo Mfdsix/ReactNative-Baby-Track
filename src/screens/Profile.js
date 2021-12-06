34550a07e22759e0b33e8a8a36c99e47b3b22a43
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-12-06 10:12:14 +0700

    finish profile screen

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -2,6 +2,33 @@ import React from 'react';
 import { ScrollView, View, Text, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
 
+import Styles from "../styles/Styles";
+import ScreenStyles from "../styles/Profile";
+
+import BottomNavigation from "../components/BottomNavigation";
+
+import PROFILES from "../data/Profiles.js";
+
 export default function Profile({navigation}){
-    return <View></View>
+    return <View>
+        <ScrollView>
+            <View>
+                <Text>Profile Type</Text>
+                <View>
+                    { PROFILES.map((profile, index) => (
+                        <View key={ 'profile-' + index }>
+                            <View>
+                                <Text>{ profile.title }</Text>
+                                <Text>{ profile.description }</Text>
+                            </View>
+                            <View>
+                                <Icon name="" type="ionicon"/>
+                            </View>
+                        </View>
+                    )) }
+                </View>
+            </View>
+        </ScrollView>
+        <BottomNavigation navigation={ navigation } index={4}/>
+    </View>
 }
